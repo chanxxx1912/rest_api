dfc5e67a75219a3180d8fb88f682f3f174bb2c53
chanxxx1912 <chandana1912harna@gamil.com>
2023-03-24 15:49:52 +0530

    second

--- server/urls.py
+++ server/urls.py
@@ -5,12 +5,16 @@ from django.conf import settings
 
 from django.contrib import admin
 from django.urls import path
-from .views import file_list ,file_create
+from .views import file_list , file_upload
 urlpatterns = [
     path('', index, name='index'),
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('list/', file_list),
-    path('post/', file_create),
+    #path('upload/', FileUploadView.as_view(), name='file_upload'),
+    path('upload/', file_upload, name='file_upload'),
+    #path('file/list/create_file/', create_file, name='create_file')
+    #path('create_file/', create_file, name='create_file')
+
     
 ]
 
--- server/views.py
+++ server/views.py
@@ -14,31 +14,38 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .serializers import MyFileSerializer
 from .models import File
-
-
-from django.shortcuts import render
-from rest_framework.response import Response
+from django.http import FileResponse
 from rest_framework.decorators import api_view
+from django.views.generic.base import View
+#from django.views.decorators.http import require_GET
 
-
-
-# Create your views here.
 @api_view(['GET'])
 def file_list(request):
     file = File.objects.all()
     serializer = MyFileSerializer(file, many = True)
     return Response(serializer.data)
 
-@api_view(['POST'])
-def file_create(request):
-    serializer = MyFileSerializer(data = request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    else:
-        return Response(serializer.errors)
-        
-    
+@api_view(['GET', 'POST'])
+def file_upload(request):
+    if request.method == 'POST':
+        serializer = MyFileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+            data = serializer.validated_data
+            result = []
+            traverse(data, result=result)
+            output = "\n".join(result)
+            path = "output.txt"
+            with open(path, "w") as file:
+                file.write(output)
+            # Return a download response
+            response = FileResponse(open(path, 'rb'), content_type='text/plain')
+            response['Content-Disposition'] = 'attachment; filename="output.txt"'
+            return response
+        return Response(serializer.validated_data)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
 def traverse(item, result, path=""):
     if isinstance(item, dict):
         for key, value in item.items():
@@ -48,13 +55,6 @@ def traverse(item, result, path=""):
             traverse(value, result, path)
     else:
         result.append(f'{path[1:]}."{item}",')
-
-
-
-   
-
-
-from django.http import FileResponse
 
 @csrf_exempt
 def index(request):
